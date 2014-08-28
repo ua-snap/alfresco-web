292b4f99f636e95e50ec010917f274c23b80c2e8
Alec Bennett <apbennett@localhost.localdomain>
2014-08-28 09:33:11 -0800

    First repo commit

---
+++ app.js
@@ -0,0 +1,48 @@
+/*
+ * * Module dependencies
+ * */
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
+
+var app = express()
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+app.use(express.bodyParser());
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+})
+app.get('/about', function (req, res) {
+  var cp = require("child_process");
+  cp.exec("touch file.x");
+  res.render('about',
+  { title : 'Home' }
+  )
+})
+
+var util = require('util');
+app.post('/launch', function(req, res){
+  	var cp = require("child_process");
+	cp.exec("touch file." + req.body.fif);
+	res.render('launched')
+});
+
+app.listen(3000)
